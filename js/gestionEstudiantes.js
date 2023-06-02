bc323f4c14cf9a0a56e5fa22608d51c8d293facd
AlexHernande2 <edalhernandez@uniboyaca.edu.co>
2023-06-02 18:43:59 -0500

    Se termino de agregarle cambios a registro de usuario  

--- js/gestionEstudiantes.js
+++ js/gestionEstudiantes.js
@@ -1,41 +1,42 @@
-$(document).ready(function(){
+$(document).ready(function () {
+    let condicionGuardar = 0;
+    let codigoBuscar = null;
     $.ajax({
         method: 'get',
-        url: 'http://localhost:8000/usuarios'
-    }).done((response)=>{
-        const dataJson= JSON.parse(response);
-        const estudiantes =dataJson.data;
+        url: 'http://localhost:8000/estudiantes'
+    }).done((response) => {
+        const dataJson = JSON.parse(response);
+        const estudiantes = dataJson.data;
         const table = document.getElementById('estudianteTb');
-        const tbody= table.getElementsByTagName('tbody')[0];
-        let html ='';
+        const tbody = table.getElementsByTagName('tbody')[0];
+        let html = '';
         estudiantes.forEach(estudiante => {
-           html+='<tr>'
-           html+='      <td>' + estudiante.codigo+'</td>';
-           html+='      <td>'+ estudiante.nombres+'</td>';
-           html+='      <td>'+ estudiante.apellidos+'</td>';
-           html+='      <td>';
-           html+='          <button class="btnModificar" data-id="' + estudiante.codigo + '" >Modificar</button>';
-           html+='      </td>';
-           html+='      <td>';
-           html+='          <button class="btnEliminar" data-id="' +  estudiante.codigo + '" >Eliminar</button>';
-           html+='      </td>';
-           html+='      <td>';
-           html+='          <button class="btnNotas" data-id="' +  estudiante.codigo + '" >Notas</button>';
-           html+='      </td>';
-           html+='</tr>';
-            
+            html += '<tr>'
+            html += '      <td>' + estudiante.codigo + '</td>';
+            html += '      <td>' + estudiante.nombres + '</td>';
+            html += '      <td>' + estudiante.apellidos + '</td>';
+            html += '      <td>';
+            html += '          <button class="btnModificar" data-codigo="' + estudiante.codigo + '" >Modificar</button>';
+            html += '      </td>';
+            html += '      <td>';
+            html += '          <button class="btnEliminar" data-codigo="' + estudiante.codigo + '" >Eliminar</button>';
+            html += '      </td>';
+            html += '      <td>';
+            html += '          <button class="btnNotas" data-codigo="' + estudiante.codigo + '" >Notas</button>';
+            html += '      </td>';
+            html += '</tr>';
+
         });
-        tbody.innerHTML= html;
-    }).fail((error)=>{
+        tbody.innerHTML = html;
+    }).fail((error) => {
         console.error(error);
     });
 
-    let condicionGuardar = 0;
-    let id = null;
+
 
     // REGISTRAR USUARIOS
 
-    $(document).on("click", "#btnRegistrar", function(){
+    $(document).on("click", "#btnRegistrar", function () {
 
         document.getElementById('titulo').innerText = 'Registrar';
         condicionGuardar = 1;
@@ -45,39 +46,43 @@ $(document).ready(function(){
 
     // MODIFICAR REGISTRO
 
-    $(document).on("click", ".btnModificar", function(){
+    $(document).on("click", ".btnModificar", function () {
 
         document.getElementById('titulo').innerText = 'Modificar';
 
         condicionGuardar = 2;
-        id = $(this).data("id"); //Guarda en una variable el id recogido en las propiedades del boton Modificar
-        var name = $(this).closest("tr").find("td:eq(0)").text(); //TR = Fila, TD = celda
-        var username = $(this).closest("tr").find("td:eq(1)").text();
-
-        $("#nameId").val(name);
-        $("#usernameId").val(username);
+        //Guarda en una variable el codigo recogido en las propiedades del boton Modificar
+        codigoBuscar = $(this).data("codigo");
+        var nombres = $(this).closest("tr").find("td:eq(1)").text(); //TR = Fila, TD = celda
+        var apellidos = $(this).closest("tr").find("td:eq(2)").text();
+        
+        $("#codigoId").val(codigoBuscar);
+        //muestra el codigo pero soamente como lectrua 
+        $("#codigoId").prop("readonly", true);
+        $("#nombreId").val(nombres);
+        $("#apellidoId").val(apellidos);
 
     });
 
     // ELIMINAR REGISTRO
 
-    $(document).on("click", ".btnEliminar", function(){
+    $(document).on("click", ".btnEliminar", function () {
 
-        id = $(this).data("id");
+        codigoBuscar = $(this).data("codigo");
 
         $.ajax({
-            url: 'http://localhost:8000/usuarios/' + id,
+            url: 'http://localhost:8000/estudiantes/' + codigoBuscar,
             method: 'delete',
-        }).done(response=>{
+        }).done(response => {
             const dataJson = JSON.parse(response);
-            const msg = dataJson.data; 
+            const msg = dataJson.data;
             alert(msg);
 
             location.reload();
 
-        }).fail(error=>{
+        }).fail(error => {
             const dataJson = JSON.parse(response);
-            const msg = dataJson.data; 
+            const msg = dataJson.data;
             alert(msg);
         });
 
@@ -87,73 +92,72 @@ $(document).ready(function(){
 
     document.getElementById('btnGuardar').addEventListener('click', guardar);
 
-    function guardar(){
+    function guardar() {
 
         //Variables a utilizar en el ajax
-        let name = document.getElementById('nameId');
-        let userName= document.getElementById('usernameId');
-        let password = document.getElementById('passwordId');
+        let codigo = document.getElementById('codigoId');
+        let nombres = document.getElementById('nombreId');
+        let apellidos = document.getElementById('apellidoId');
 
 
         //Variables para validar que los input no puedan estar vacios
-        let nameVer = document.getElementById('nameId').value;
-        let userNameVer= document.getElementById('usernameId').value;
-        let passwordVer = document.getElementById('passwordId').value;
+        let codVer = document.getElementById('codigoId').value;
+        let nombreVer = document.getElementById('nombreId').value;
+        let apellidodVer = document.getElementById('apellidoId').value;
 
 
-        if(condicionGuardar == 0){
+        if (condicionGuardar == 0) {
             alert("Debe seleccionar una funcionalidad para el botón");
             return;
-        }else if(nameVer.trim() === '' || userNameVer.trim() === '' || passwordVer.trim() === ''){
-            alert ("No pueden haber campos vacios");
+        } else if (codVer.trim() === '' || nombreVer.trim() === '' || apellidodVer.trim() === '') {
+            alert("No pueden haber campos vacios");
             return
         }
 
-        if(condicionGuardar == 1){
+        if (condicionGuardar == 1) {
             $.ajax({
-                url: 'http://localhost:8000/usuarios',
+                url: 'http://localhost:8000/estudiantes',
                 method: 'post',
                 data: {
-                    name: name.value,
-                    username: userName.value,
-                    password: password.value
+                    codigo: codigo.value,
+                    nombres: nombres.value,
+                    apellidos: apellidos.value
                 }
             }).done(response => {
                 const dataJson = JSON.parse(response);
                 const msg = dataJson.data;
                 alert(msg)
+
+
+            });
+        } else if (condicionGuardar == 2) {
+            $.ajax({
+                url: 'http://localhost:8000/estudiantes/' + codigoBuscar,
+                method: 'put',
+                data: {
+                    nombres: nombres.value,
+                    apellidos: apellidos.value
+                }
+            }).done(response => {
+                const dataJson = JSON.parse(response);
+                const msg = dataJson.data;
+                alert(msg);
                 location.reload();
 
             });
-        }else if(condicionGuardar == 2){
-            $.ajax({
-                url: 'http://localhost:8000/usuarios/' + id,
-                method: 'put',
-                data:{
-                         name: name.value,
-                         username: userName.value,
-                         password: password.value
-                     }
-                 }).done(response=>{
-                     const dataJson = JSON.parse(response);
-                     const msg = dataJson.data; 
-                     alert(msg);
-                    location.reload();
-                     
-                 });
         }
 
     }
 
-    function clean(){
-        let name = "";
-        let userName= "";
-        let password = "";
+    function clean() {
+        let codigo = "";
+        let nombres = "";
+        let apellidos = "";
 
-        document.getElementById("nameId").value = name;
-        document.getElementById("usernameId").value = userName;
-        document.getElementById("passwordId").value = password;
+        document.getElementById("codigoId").value = codigo;
+        document.getElementById("nombreId").value = nombres;
+        document.getElementById("apellidoId").value = apellidos;
 
     };
 
-  });
+});
